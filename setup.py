9382796ae2570e467c631ae9c637704050f25dc3
quamrul hoda <qhoda489@gmail.com>
2024-09-29 15:17:13 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+  name='scrapper',
+  version='0.0.1',
+  author='pwskills',
+  author_email='quamrul21@gmail.com',
+  packages=find_packages(),
+  install_requires=[]
+)
